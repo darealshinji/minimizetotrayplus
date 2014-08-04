9689594ef8387c24ecb0e00f30577e1b3e599cd4
djcj <djcj@gmx.de>
2014-08-05 01:24:45 +0200

    remove original code from master branch

--- original/minimizetotray-r59-source/makefiles.sh
+++
@@ -1,6 +0,0 @@
-add_makefiles "
-  extensions/minimizetotray/Makefile
-  extensions/minimizetotray/source/Makefile
-  extensions/minimizetotray/source/linux/Makefile
-  extensions/minimizetotray/source/windows/Makefile
-"
